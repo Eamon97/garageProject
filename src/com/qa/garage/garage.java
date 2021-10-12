6abc2254fed844d02196f7040feb7462a929b615
eamonmoradi <eamonmoradi@192.168.0.20>
2021-10-12 16:18:09 +0100

    added garage class to implement code methods

---
+++ src/com/qa/garage/garage.java
@@ -0,0 +1,31 @@
+package com.qa.garage;
+
+public class garage {
+	
+	public static void calculateBill() {
+		
+		for(Vehicle:garage) {
+			if(Vehicle.productionYear>=2020) {
+				int bill=200;
+				return bill
+			}
+			else {int bill=150;
+			return bill
+				}
+			}
+		}
+		
+		public static void removeVehicle() {
+			
+			
+		}
+		public static void fixVehicle(Vehicle[]) {
+			System.out.println(Vehicle[] + " has been fixed");
+			
+		}
+		
+		public static void clearGarage() {
+			garage.clear();		}
+		
+	}
+}
--- src/com/qa/garage/runner.java
+++ src/com/qa/garage/runner.java
@@ -30,30 +30,6 @@ public class runner {
 		
 		
 		
-		public static void calculateBill() {
-		for(Vehicle:garage) {
-			if(Vehicle.productionYear>=2020) {
-				int bill=200;
-				return bill
-			}
-			else {int bill=150;
-			return bill
-				}
-			}
-		}
-		
-		public static void removeVehicle() {
-			
-			
-		}
-		public static void fixVehicle(Vehicle[]) {
-			System.out.println(Vehicle[] + " has been fixed");
-			
-		}
-		
-		public static void clearGarage() {
-			garage.clear();		}
-		
 	}
 }
 
